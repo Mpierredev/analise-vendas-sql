b8c4676b8c5b3425b37bb47fc133762923fb75df
Márcio Pierre <marciopierresantos@gmail.com>
2026-02-01 21:27:04 -0300

    Update analise_vendas.sql

--- analise_vendas.sql
+++ analise_vendas.sql
@@ -21,3 +21,17 @@ SELECT
 FROM "retail_sales_dataset"
 GROUP BY mes
 ORDER BY mes;
+
+-- Análise de comportamento de compra por faixa etária e categoria
+SELECT 
+    CASE 
+        WHEN "Age" BETWEEN 18 AND 30 THEN '18-30 (Jovens)'
+        WHEN "Age" BETWEEN 31 AND 50 THEN '31-50 (Adultos)'
+        ELSE '50+ (Sênior)'
+    END AS faixa_etaria,
+    "Product Category",
+    SUM("Total Amount") AS total_gasto,
+    COUNT(*) AS total_transacoes
+FROM "retail_sales_dataset"
+GROUP BY faixa_etaria, "Product Category"
+ORDER BY faixa_etaria, total_gasto DESC;
